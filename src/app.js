5f3b4e020753d676fffbc8162e5431bef434b5ff
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-09-15 10:42:30 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -11,7 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || '*'
+        origin: function (origin, callback) {
+        const allowedOrigins = [
+            process.env.FRONTEND_URL,
+            "http://localhost:5173"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS not allowed"));
+        }
+        },
+        credentials: true,
     })
 );
 
